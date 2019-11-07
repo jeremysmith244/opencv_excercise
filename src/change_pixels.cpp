aeb18378e665579b994927929bfa319753b9ef9c
root <jeremysmith244@gmail.com>
2019-11-06 19:46:11 -0800

    completed 1 and 2

--- src/change_pixels.cpp
+++ src/change_pixels.cpp
@@ -13,10 +13,11 @@ void changePixels()
     m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
     m1_8u.setTo(cv::Scalar(0));          //black
 
-    for (int r = 230; r < 250; r++)
-    {
+    for (int r = 230; r < 250; r++) {
+        for (int c = 0; c < 640; c++) {
+            m1_8u.at<uchar>(r, c) = 255;
         // STUDENT TASK : loop over all columns and set matrix elements to 255
-
+        }
     }
 
     // show result
--- src/create_matrix.cpp
+++ src/create_matrix.cpp
@@ -9,9 +9,9 @@ void createMatrix1()
 {
     // create matrix
     int nrows = 480, ncols = 640;
-    cv::Mat m1_8u;
-    m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
-    m1_8u.setTo(cv::Scalar(255));        // white
+    cv::Mat m3_8u;
+    m3_8u.create(nrows, ncols, CV_8UC3); // two-channel matrix with 8bit unsigned elements
+    m3_8u.setTo(cv::Scalar(255,0,0));        // white
 
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
@@ -21,7 +21,7 @@ void createMatrix1()
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
     cv::namedWindow(windowName, 1); // create window
-    cv::imshow(windowName, m1_8u);
+    cv::imshow(windowName, m3_8u);
     cv::waitKey(0); // wait for keyboard input before continuing
 
     // STUDENT TASK :
